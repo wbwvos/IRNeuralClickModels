d1a46e4769b3276d37dcf318f88bdd2ffd40acd8
Jeroen Rooijmans <jhmrooijmans@gmail.com>
2016-11-14 15:28:08 +0100

    improved data generator

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,14 +1,36 @@
 import numpy as np
 import cPickle
 
-num_items = 200
-data = []
+batch_size = 64
+serp_len = 10 # SERP contains 10 documents
 
-for i in range(num_items):
-    # create SERP
-    serp = np.random.permutation(10)
-    # create data vector of length 10240
-    vec = np.random.choice(2, size=10, p=(0.9, 0.1))
-    data.append((serp, vec))
 
-np.save('data.npy', data)
+# function that creates data example
+def create_example(q, prev_i):
+    # create document vector d
+    d = np.round(np.maximum(0, 2.*np.random.randn(10240)))
+    # create interaction vector i
+    i = np.random.choice(2, p=(0.75, 0.25))
+    return np.append(np.append(q, d), np.append(prev_i, i)).T
+
+
+def create_serp(serp_len):
+    q = 0
+    i = 0
+    serp = np.zeros((serp_len, 10243))
+    for j in range(serp_len):
+        example = create_example(q, i)
+        i = example[-1]
+        serp[j, :] = example
+    return serp
+
+
+def create_batch(batch_size, serp_len):
+    batch = np.zeros((batch_size, serp_len, 10243))
+    for j in range(batch_size):
+        serp = create_serp(serp_len)
+        batch[j, :, :] = serp
+    return batch
+
+batch = create_batch(batch_size, serp_len)
+np.save('batch.npy', batch)
--- src/lstm.py
+++ src/lstm.py
@@ -18,30 +18,19 @@ np.random.seed(7)
 
 def load_data():
     # import dataset
-    data = np.load('data.npy')
-    split1 = np.array_split(data, 10)
-    split2 = np.concatenate(split1[:9])
-    test = split1[9]
-    split3 = np.array_split(split2, 10)
-    train = np.concatenate(split3[:9])
-    val = split3[9]
-    return (train[:, 0], train[:, 1]), (val[:, 0], val[:, 1]), (test[:, 0],
-                                                                test[:, 1])
+    data = np.load('batch.npy')
+    return (data[:, :, :-1], data[:, :, -1])
 
 # load data
-(train_X, train_y), (val_X, val_y), (test_X, test_y) = load_data()
+(train_X, train_y) = load_data()
 print('Loaded data!')
-print('Train data shape: {0}').format(str(train_X.shape))
-print('Validation data shape: {0}').format(str(val_X.shape))
-print('Test data shape: {0}').format(str(test_X.shape))
+print('Train data shape: ({0},{1})').format(str(train_X.shape),
+                                            str(train_y.shape))
 
-print train_X.shape, train_y.shape
-print train_X, train_y
 # DEFINE LSTM MODEL #
 # create model
 model = Sequential()
-model.add(Embedding(10, 10, input_length=train_X.shape[0]))
-model.add(LSTM(100), dropout_W=0.2, dropout_U=0.2)
+model.add(LSTM(256, batch_input_shape=(1, 10, 10242)))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam',
               metrics=['accuracy'])
@@ -49,9 +38,4 @@ model.compile(loss='binary_crossentropy', optimizer='adam',
 print model.summary()
 
 # train model
-model.fit(train_X, train_y, validation_data=(val_X, val_y), nb_epoch=1,
-          batch_size=32)
-
-# Final evaluation of the model
-scores = model.evaluate(test_X, test_y, verbose=1)
-print('Accuracy: {.2f}').format(scores[1]*100)
+model.fit(train_X, train_y, nb_epoch=5, batch_size=64)
