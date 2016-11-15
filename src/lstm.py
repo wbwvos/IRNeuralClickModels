adbfbd770d9cd8d1d23d195fc87245608b59dfdf
Wolf Vos <wbwvos@gmail.com>
2016-11-15 19:23:28 +0100

    lstm working rather good on small dataset 395 samples 95 test accuracy

--- src/lstm.py
+++ src/lstm.py
@@ -8,29 +8,49 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM, TimeDistributed
 import time
 import os.path
-import pickle
+#import pickle
+import cPickle as pickle
+from scipy.sparse import coo_matrix
+
 # since we are using stateful rnn tsteps can be set to 1
 
 tsteps = 1
 batch_size = 64
-epochs = 300
+epochs = 200
 serp_len = 10
 inputs = 10242
 num_hidden = 256
 output = 1
 # number of elements ahead that are used to make the prediction
-x_train_e = np.load('data.npy')
-y_train_e = np.load('labels.npy')
-#y_train_e = np.reshape(y_train_e, (batch_size, serp_len))
+#x_train = np.load('data.npy')
+#y_train = np.load('labels.npy')
 
-print(x_train_e.shape, y_train_e.shape)
+with open('data_list.cpickle', 'rb') as f:
+    data = pickle.load(f)
+
+x_data = np.zeros((395, 10, 10242))
+y_data = np.zeros((395, 10, 1))
+for i, d in enumerate(data):
+    matrix = d.todense()
+    x_data[i, :, 1:] = matrix[:, :-1]
+    y_data[i, :] = matrix[:, -1]
+
+x_train = x_data[:300, :, :]
+y_train = y_data[:300, :, :]
+x_val = x_data[300:, :, :]
+y_val = y_data[300:, :, :]
+
+print('x_train:', x_train.shape)
+print('y_train:', y_train.shape)
+print('x_val:', x_val.shape)
+print('y_val:', y_val.shape)
 
 print('Creating Model')
 model = Sequential()
 model.add(TimeDistributed(Dense(num_hidden), input_shape=(serp_len, inputs)))
 model.add(LSTM(num_hidden, return_sequences=True))
-model.add(TimeDistributed(Dense(1)))
-model.compile(loss='mse', optimizer='rmsprop')
+model.add(TimeDistributed(Dense(1, activation='sigmoid')))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 model.summary()
 weights_filename = 'weights.dat'
 
@@ -42,19 +62,20 @@ else:
     trainStart = time.time()
     for i in range(epochs):
         print('Epoch', i+1, '/', epochs)
-        model.fit(x_train_e,
-                  y_train_e,
-                  batch_size=4,
+        model.fit(x_train,
+                  y_train,
+                  batch_size=64,
                   verbose=1,
                   nb_epoch=1,
-                  shuffle=False)
-        model.reset_states()
+                  shuffle=True)
+        #model.reset_states()
     trainEnd = time.time()
     print('Trained the model in', trainEnd - trainStart, 'seconds')
     print('Saving the model...')
     model.save_weights(weights_filename, True)
 
 
-print('Predicting')
-predicted = model.predict(x_train_e, batch_size=batch_size, verbose=True)
-print(predicted)
+print('Evaluating')
+score = model.evaluate(x_val, y_val, batch_size=batch_size, verbose=1)
+print('log likelihood:  ', score[0])
+print('Accuracy:        ', score[1])
