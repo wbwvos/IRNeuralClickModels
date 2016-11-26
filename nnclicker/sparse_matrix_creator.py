643f13c3a08b9adb75fdde5793de8d33f8866cea
alexandervansomeren <alexander.vansomeren@gmail.com>
2016-11-26 15:06:11 +0100

    Added save functionality for sparse matrix creator

--- nnclicker/sparse_matrix_creator.py
+++ nnclicker/sparse_matrix_creator.py
@@ -6,11 +6,11 @@ __author__ = 'Wolf Vos, Casper Thuis, Alexander van Someren, Jeroen Rooijmans'
 
 
 class SparseMatrixGenerator:
-    def __init__(self, representation_set='1', fname):
+    def __init__(self, fname='query_docs_queries_docs_100000.json'):
 
         # Open the json file
         with open(fname, 'r') as f:
-            data = json.load(f)
+            data = pickle.load(f)
 
         # Separate the document vectors and query vectors for set 2 and 3
         self.docs = data.pop('docs')
@@ -19,17 +19,15 @@ class SparseMatrixGenerator:
         # Obtain the query dict and save occurences of indices
         self.query_dicts = data.pop('query_docs')
 
-        # Set the representation set
-        self.representation_set = representation_set
-
         # Delete unused variables
         del data
         # del query_dict
 
-    def get_sparse_matrices(self, query_id='20369649'):
+    def get_sparse_matrices(self, query_id='20369649', representation_set='1'):
         """
         Returns a list of sparse matrices (one for each serp within the query id)
-        :param query_id:
+        :param representation_set: Representation set as described in the paper
+        :param query_id: The query ID as provided by the Yandex dataset
         :return list of sparse matrices:
         """
         sparse_matrices = []
@@ -38,29 +36,29 @@ class SparseMatrixGenerator:
         doc_indices = query_dict
 
         q_indices = []
-        if self.representation_set != '1':
+        if representation_set != '1':
             # add q vector to indices
             q_indices = [val + 10240 for val in self.queries[query_id]]
 
         for serp in serps:
-            sparse_matrices.append(self.get_sparse_matrix_from_serp(serp, doc_indices, q_indices))
+            sparse_matrices.append(self.get_sparse_matrix_from_serp(serp, doc_indices, q_indices, representation_set))
         return sparse_matrices
 
-    def get_sparse_matrix_from_serp(self, serp, doc_indices, q_indices):
+    def get_sparse_matrix_from_serp(self, serp, doc_indices, q_indices, representation_set):
         data = []
         rows = []
         cols = []
 
         # Append q indices for set 2
-        if self.representation_set != '1':
+        if representation_set != '1':
             q_index_counts = {index: q_indices.count(index) for index in q_indices}
             data.extend(q_index_counts.values() * 10)
             cols.extend(q_index_counts.keys() * 10)
-            rows.extend([j for j in range(10) for i in range(len(q_index_counts))])
+            rows.extend([j for j in range(10) for _ in range(len(q_index_counts))])
 
         # Append qd vector indices and d indices for set 3
         for row, doc_id in enumerate(serp['doc_ids']):
-            if self.representation_set == '3':
+            if representation_set == '3':
                 doc_indices.extend([val + 10240 + 1024 for val in self.docs[doc_id]])
             index_counts = {index: doc_indices[doc_id].count(index) for index in doc_indices[doc_id]}
             data.extend(index_counts.values())
@@ -68,11 +66,11 @@ class SparseMatrixGenerator:
             rows.extend([row] * len(index_counts.keys()))
 
         # Append interaction and label indices
-        if self.representation_set == '1':
-            interaction_index = 10240
-        if self.representation_set == '2':
+        # if representation_set == '1':
+        interaction_index = 10240
+        if representation_set == '2':
             interaction_index = 10240 + 1024
-        if self.representation_set == '3':
+        if representation_set == '3':
             interaction_index = 10240 + 1024 + 10240
 
         interaction_row_indices = [i for i, j in enumerate(serp['click_pattern']) if j == 1]
@@ -86,3 +84,10 @@ class SparseMatrixGenerator:
         cols.extend([interaction_index + 1] * len(label_row_indices))
 
         return csr_matrix((data, (rows, cols)), shape=(10, interaction_index + 2))
+
+    def save_matrices_to_file(self, fname='sparse_matrices.cpickle', representation_set='1'):
+        sparse_matrices = []
+        for query_id in self.queries.keys():
+            sparse_matrices.extend(self.get_sparse_matrices(query_id, representation_set))
+        with open(fname, 'r') as f:
+            pickle.dump(sparse_matrices, f)
