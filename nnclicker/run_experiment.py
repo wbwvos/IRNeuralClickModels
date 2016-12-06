3bbc027d2b3d2513afb0a70f272087fd3355b4cb
alexandervansomeren <alexander.vansomeren@gmail.com>
2016-12-06 12:45:07 +0100

    Run experiment update

--- nnclicker/run_experiment.py
+++ nnclicker/run_experiment.py
@@ -43,47 +43,50 @@ def run_experiment():
     logging.info("creating NNclickParser...")
     parser = preprocess.NNclickParser()
 
-    # session preprocessing
-    if os.path.isfile(data_dir+session_name):
-        logging.info("found session file: %s" % (data_dir+session_name))
-        if not parser.query_sessions:
-            logging.info("loading session file...")
-            parser.load_sessions(data_dir+session_name)
-    else:
-        logging.info("parsing session %d to %d from file: %s" % (
-            sessions_start, sessions_max, data_dir+datafile))
-        parser.parse(data_dir+datafile, sessions_start=sessions_start,
-                     sessions_max=sessions_max)
-        logging.info("writing session file: %s" % data_dir+session_name)
-        parser.write_sessions(data_dir+session_name)
-
-    # query docs preprocessing
-    if os.path.isfile(data_dir+query_name):
-        logging.info("found query docs file: %s" % (data_dir+query_name))
-        if not parser.query_docs:
-            logging.info("loading query docs file...")
-            parser.load_query_docs(data_dir+query_name)
-    else:
-        if not parser.query_sessions:
-            logging.info("loading session file...")
-            parser.load_sessions(data_dir+session_name)
-        logging.info("parsing query docs from file: %s" % (
-            data_dir+session_name))
-        parser.create_data_dicts()
-        logging.info("deleting parser.sessions from memory...")
-        # del parser.sessions
-        logging.info("writing query docs file: %s" % (data_dir+query_name))
-        parser.write_query_docs(data_dir+query_name)
+    # # session preprocessing
+    # if os.path.exists(data_dir+session_name):
+    #     logging.info("found session file: %s" % (data_dir+session_name))
+    #     if not parser.query_sessions:
+    #         logging.info("loading session file...")
+    #         parser.load_sessions(data_dir+session_name)
+    # else:
+    #     logging.info("parsing session %d to %d from file: %s" % (
+    #         sessions_start, sessions_max, data_dir+datafile))
+    #     parser.parse(data_dir+datafile, sessions_start=sessions_start,
+    #                  sessions_max=sessions_max)
+    #     logging.info("writing session file: %s" % data_dir+session_name)
+    #     parser.write_sessions(data_dir+session_name)
+    #
+    # # query docs preprocessing
+    # if os.path.exists(data_dir+query_name):
+    #     logging.info("found query docs file: %s" % (data_dir+query_name))
+    #     if not parser.query_docs:
+    #         logging.info("loading query docs file...")
+    #         parser.load_query_docs(data_dir+query_name)
+    # else:
+    #     if not parser.query_sessions:
+    #         logging.info("loading session file...")
+    #         parser.load_sessions(data_dir+session_name)
+    #     logging.info("parsing query docs from file: %s" % (
+    #         data_dir+session_name))
+    #     parser.create_data_dicts()
+    #     logging.info("deleting parser.sessions from memory...")
+    #     # del parser.sessions
+    #     logging.info("writing query docs file: %s" % (data_dir+query_name))
+    #     parser.write_query_docs(data_dir+query_name)
 
     # sparse matrix preprocessing
-    if os.path.isfile(data_dir+sparsematrix_name):
+    if os.path.exists(data_dir + sparsematrix_name):
         logging.info("found sparse matrix file: %s" % (
             data_dir+sparsematrix_name))
+        print "found sparse matrix file: %s" % (
+            data_dir + sparsematrix_name)
     else:
         logging.info("creating SparseMatrixGenerator...")
+        print "creating SparseMatrixGenerator..."
         sparse_matrix_gen = SparseMatrixGenerator(fname=data_dir+query_name)
         logging.info("generating sparse matrixes...")
-        sparse_matrix_gen.save_matrices_to_file(
+        sparse_matrix_gen.save_matrices_to_files(
             fname=data_dir+sparsematrix_name, representation_set=repr_set)
 
 if __name__ == "__main__":
