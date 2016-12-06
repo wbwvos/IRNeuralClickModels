4cf29dd207e05cdd781f5c2e19a82f77cf232b60
alexandervansomeren <alexander.vansomeren@gmail.com>
2016-12-06 10:53:00 +0100

    Added list_batch_writer

--- nnclicker/utils.py
+++ nnclicker/utils.py
@@ -47,18 +47,12 @@ def dict_batch_writer(dictionary, fname, number_of_batches=10):
             if exc.errno != errno.EEXIST:
                 raise
     total_size = len(dictionary.keys())
-    batch_number = 0
-    # for batch in chunks(dictionary, SIZE=number_of_batches):
-    #     with open(os.path.join(fname, str(batch_number) + ".pickle"), 'w') as f:
-    #         pickle.dump(batch, f)
-    #     batch_number += 1
-
     for batch_number in bar(range(number_of_batches)):
         start = total_size / number_of_batches * batch_number
         end = total_size / number_of_batches * (batch_number + 1)
         with open(os.path.join(fname, str(batch_number) + ".pickle"), 'w') as f:
-            pickle.dump({key: dictionary[key] for key in dictionary.keys()[start:end]}, f)
-    print "Done."
+            pickle.dump({key: dictionary.pop(key) for key in dictionary.keys()[start:end]}, f, -1)
+    print "Done. Succesfully wrote " + str(batch_number + 1) + " batches."
 
 
 def dict_batch_reader(fname):
@@ -72,3 +66,29 @@ def dict_batch_reader(fname):
         with open(filename, 'r') as f:
             dictionary.update(pickle.load(f))
     return dictionary
+
+
+def list_batch_writer(fname, output_list, batch_size=64):
+    print "Batch writing " + fname + "..."
+
+    if not os.path.exists(fname):
+        try:
+            os.makedirs(fname)
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+    total_size = len(output_list)
+
+    bar = progressbar.ProgressBar(maxval=total_size / batch_size,
+                                  widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+
+    for batch_number in bar(range(total_size / batch_size)):
+        start = batch_number * batch_size
+        end = (batch_number + 1) * batch_size
+
+        if end > total_size:
+            end = total_size
+
+        with open(os.path.join(fname, str(batch_number) + ".pickle"), 'w') as f:
+            pickle.dump(output_list[start:end], f, -1)
+    print "Done. Succesfully wrote " + str(batch_number) + " batches of size " + str(batch_size) + "."
